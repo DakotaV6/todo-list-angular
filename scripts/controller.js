346762c200e17f194a11372e02c0987ebe532804
Dakota <dakota.vradenburg@gmail.com>
2018-11-02 16:37:08 -0400

    Added full functionality. Filtering now works.

--- scripts/controller.js
+++ scripts/controller.js
@@ -16,8 +16,20 @@ function TodoController() {
         task: "Eat lunch",
         completed: true
     }];
+
     vm.addTask = (newTask) => {
-        vm.tasks.push(angular.copy(newTask));
+        vm.tasks.push(angular.copy({task: newTask, completed: false}));
+        // console.log(vm.tasks);
+    }
+
+    vm.completeTask = (task) => {
+        task.completed = true;
+    }
+    
+    vm.deleteTask = (task) => {
+        // console.log(task);
+        let index = vm.tasks.indexOf(task);
+        vm.tasks.splice(index, 1);
     }
 }
 
