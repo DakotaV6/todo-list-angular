58853fc319bc2364046bddbe233d550111106253
Dakota <dakota.vradenburg@gmail.com>
2018-11-02 15:09:27 -0400

    Finished Part One. Styling done, Add button fully functional, and ng commands started.

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,7 +3,19 @@
 function TodoController() {
     const vm = this;
 
-    vm.tasks = [];
+    vm.tasks = [{
+        task: "Homework",
+        completed: true
+    }, {
+        task: "Walk the dog",
+        completed: false
+    }, {
+        task: "Laundry",
+        completed: false
+    }, {
+        task: "Eat lunch",
+        completed: true
+    }];
     vm.addTask = (newTask) => {
         vm.tasks.push(angular.copy(newTask));
         console.log(vm.tasks);
